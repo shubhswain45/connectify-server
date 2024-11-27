9e53efa7b05d6c6e3125fc3e69ec0c977f815121
shubhswain45 <shubhkantswain@.com>
2024-11-27 10:26:14 +0530

    basic graphql server setup

---
+++ src/app/index.ts
@@ -0,0 +1,44 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import bodyParser from 'body-parser';
+
+export async function initServer() {
+    const app = express();
+
+    // CORS configuration
+    const corsOptions = {
+        origin: ['http://localhost:3000'], // your frontend URL
+        credentials: true, // Ensure cookies are sent with cross-origin requests
+    };
+
+    // Use CORS middleware
+    app.use(cors(corsOptions));
+    app.use(bodyParser.json({ limit: "10mb" }))
+
+    const graphqlServer = new ApolloServer({
+        typeDefs: `
+            type Query {
+                sayHello:String
+            }
+        `,
+        resolvers: {
+            Query: {
+                sayHello: () => "Hello"
+            },
+        },
+    });
+
+    await graphqlServer.start();
+
+    // GraphQL Middleware
+    app.use(
+        '/graphql',
+        // @ts-ignore
+        expressMiddleware(graphqlServer)
+    );
+
+
+    return app;
+}
